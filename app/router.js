6046085b917ec462c4aefacebb2808ef04d3e16b
Jéssica <jessica@systemar.com.br>
2024-02-20 11:45:14 -0300

    add arquivos do projeto - modelos e roteamento normal e aninhado

--- app/router.js
+++ app/router.js
@@ -6,4 +6,14 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('cart');
+  this.route('admin', function () {
+    this.route('add');
+    this.route('edit', {path: '/edit/:id'});
+  });
+  this.route('category', {path: '/category/:id'}, function () {
+    this.route('item', {path: 'item/:itemkey'});
+  });
+  this.route('not-found', { path: '/*path' });
+});
---
+++ app/routes/category.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+import { products } from '../data/products';
+
+export default class CategoryRoute extends Route {
+    model(params){
+        return products.filter(({category_id}) => category_id == params.id);
+    }
+}
